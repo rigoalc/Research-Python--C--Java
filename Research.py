33001c2edb40e8a4d4e1a3ceb2babca235d4d2a2
Rigo Alcover <rodigomartinalcover@gmail.com>
2021-09-19 12:04:32 -0400

    Create Research.py
    create file for submit

---
+++ Research.py
@@ -0,0 +1,55 @@
+# Rodrigo Alcover
+# 08/09/2021
+# CIS-216-12292 
+# Python Programming
+
+
+#Requirements for credit (all must be satisfied):
+#Header with your name, date, CRN, and class name
+#1 paragraph summarizing Python
+#1 paragraph summarizing C
+#1 paragraph summarizing Java
+#For each summary paragraph:
+#Who designed the language?
+#When was it first created?
+#What is the language used for?
+#2-3 paragraphs explaining the similarities and differences between these program languages
+#Your paper should briefly explain which languages are interpreted, and which are compiled, and what the difference is
+#All paragraphs are 3-5 sentences
+
+
+print('''
+Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. 
+It's high-level built-in data structures, combined with dynamic typing and dynamic binding, 
+make it very attractive for Rapid Application Development, as well as for use as a scripting or glue language to connect existing components. 
+Python is simple, the easy-to-learn syntax emphasizes readability and therefore reduces the cost of program maintenance.
+Was designed by: Guido van Rossum, and First appeared: February 20, 1991; 30 years ago.
+
+C is a general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system.
+By design, C provides constructs that map efficiently to typical machine instructions. C is highly portable and is used for scripting system applications 
+which form a major part of Windows, UNIX, and Linux operating systems.
+Was designed by: Dennis Ritchie, and was first created in 1972; 49 years ago.
+
+Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.
+Java is intended to let application developers write once, run anywhere (WORA), meaning that compiled Java code can run on all platforms 
+that support Java without the need for recompilation.
+Was designed by: James Gosling, and was first created on May 23, 1995; 26 years ago.
+
+In C code length is a bit lesser, 1.5 times less than JAVA. JAVA has quite a huge code. And with Python smaller code length, 3-4 times less than JAVA.
+C Supports Operator overloading. JAVA Does not support Operator Overloading. Python Supports Operator overloading.
+C Provide both single and multiple inheritances. JAVA Provides partial multiple inheritances using interfaces. Python Provide both single and multiple inheritances
+C Functions and variables are used outside the class. JAVA Every bit of code is inside a class. Python Functions and variables can be declared and used outside the class also.
+C program is a fast compiling programming language. JAVA Program compiler is a bit slower than C. For Python Due to the use of an interpreter execution is slower.
+
+In Java, programs are not compiled into executable files; they are compiled into bytecode (as discussed earlier), which the JVM (Java Virtual Machine) then interprets/executes at runtime. 
+Java source code is compiled into bytecode when we use the javac compiler.
+C is what is called a compiled language. This means that once you write your C program, 
+you must run it through a C compiler to turn your program into an executable that the computer can run (execute).
+Python is an interpreted language, which means the source code of a Python program is converted into bytecode that is then executed by the Python virtual machine.
+
+The difference between an interpreted and a compiled language lies in the result of the process of interpreting or compiling. 
+An interpreter produces a result from a program, while a compiler produces a program written in assembly language.
+In a compiled implementation, the original program is translated into native machine instructions, which are executed directly by the hardware.
+In an interpreted implementation, the original program is translated into something else. Another program, called "the interpreter", then examines "something else" and performs whatever actions are called for. 
+Depending on the language and its implementation, there are a variety of forms of "something else". From more popular to less popular, "something else" might be
+''')
